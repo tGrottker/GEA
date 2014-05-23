e85fe27c740985ff7ee7c2bc5bad6d4a9ad8f0b7
Thomas Grottker <s52388@beuth-hochschule.de>
2014-05-23 05:57:03 +0200

    implemented basic movement

---
+++ Minesweeper_3D/Assets/Scripts/PersonMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool stepForward = Input.GetKeyDown (KeyCode.W);
+		bool turnLeft = Input.GetKeyDown (KeyCode.A);
+		bool turnRight = Input.GetKeyDown (KeyCode.D);
+
+		if (stepForward) {
+			print("step");
+			iTween.MoveAdd(gameObject, new Vector3(0f, 0f, -1f), 10f);
+		}
+		if (turnLeft) {
+			print ("left");
+			iTween.RotateBy(gameObject, iTween.Hash("y", 0.25f, "time", 10f));
+		}
+		if (turnRight) {
+			print ("right");
+			iTween.RotateBy(gameObject, iTween.Hash("y", -0.25f, "time", 10f));
+		}
+	}
+}
--- Minesweeper_3D/Assets/Scripts/PlayerMovement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PlayerMovement : MonoBehaviour {
-
-	public float turnSmoothing = 15f;
-	public float dampTime = 0.1f;
-
-}
