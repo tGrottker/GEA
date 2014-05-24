b59dea4a452cf5c8e43358537e0dbfc21c591a25
Thomas Grottker <s52388@beuth-hochschule.de>
2014-05-24 12:27:58 +0200

    added beginning of smoother movement

--- Minesweeper_3D/Assets/Scripts/PersonMovement.cs
+++ Minesweeper_3D/Assets/Scripts/PersonMovement.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class PersonMovement : MonoBehaviour {
 
+	private Vector3 position;
+	private Vector3 forward;
+
 	// Use this for initialization
 	void Start () {
-
+		position = gameObject.transform.position;
+		forward = gameObject.transform.forward;
 	}
 	
 	// Update is called once per frame
@@ -15,8 +19,9 @@ public class PersonMovement : MonoBehaviour {
 		bool turnRight = Input.GetKeyDown (KeyCode.D);
 
 		if (stepForward) {
-			print("step");
-			iTween.MoveAdd(gameObject, new Vector3(0f, 0f, -1f), 10f);
+			position = position + forward.normalized;
+			iTween.MoveTo(gameObject, iTween.Hash("position", position, "time", 3f, "orienttopath", true, "lookahead", 0));
+			//iTween.MoveAdd(gameObject, new Vector3(0f, 0f, -1f), 10f);
 		}
 		if (turnLeft) {
 			print ("left");
