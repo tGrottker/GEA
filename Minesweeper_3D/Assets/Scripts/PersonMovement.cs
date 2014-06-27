9ef5344bdff3558fc0b1a7b4402a8de783ee7a81
Maximilian Zeigert <madalga07@hotmail.de>
2014-06-27 09:20:44 +0200

    created and added FlagSprite for Minimap

--- Minesweeper_3D/Assets/Scripts/PersonMovement.cs
+++ Minesweeper_3D/Assets/Scripts/PersonMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PersonMovement : MonoBehaviour
 {
 
+<<<<<<< HEAD
 		public GameObject plane;
 		public GameObject flagPrefab;
 		private Vector3 position;
@@ -12,6 +13,18 @@ public class PersonMovement : MonoBehaviour
 		private bool readyForMovement;
 		private float nextMove;
 		private PauseMenu pauseMenu;	
+=======
+	public GameObject plane;
+	public GameObject flagPrefab;
+	public GameObject flagSprite;
+
+	private Vector3 position;
+	private Vector3 view;
+	private GameGrid grid;
+	private bool readyForMovement;
+	private float nextMove;
+	private bool paused;
+>>>>>>> created and added FlagSprite for Minimap
 
 		// Use this for initialization
 		void Start ()
@@ -37,6 +50,7 @@ public class PersonMovement : MonoBehaviour
 						return;
 				}
 
+<<<<<<< HEAD
 				if (nextMove < Time.time) {
 						if (stepForward) {
 								nextMove = nextMove + 1.2f;
@@ -87,6 +101,62 @@ public class PersonMovement : MonoBehaviour
 					flagObject.transform.parent = GameObject.FindWithTag ("GameArea").GetComponent<Transform> ();*/
 								//flagObject.animation.Play();
 						}
+=======
+		if (!paused) {
+			if (nextMove < Time.time) {
+				if (stepForward) {
+					nextMove = nextMove + 1.2f;
+					position = position + view;
+					iTween.MoveTo (gameObject, iTween.Hash ("position", position, "time", 1f, "orienttopath", false, "lookahead", 0f, "easetype", "easeInOutQuad"));
+				} else if (stepBack) {
+					nextMove = nextMove + 1.2f;
+					position = position - view;
+					iTween.MoveTo (gameObject, iTween.Hash ("position", position, "time", 1f, "orienttopath", false, "lookahead", 0f, "easetype", "easeInOutQuad"));
+				} else if (turnLeft) {
+					nextMove = nextMove + 1.7f;
+					Vector3 viewn = -gameObject.transform.right.normalized;
+					position = position + (view + viewn) / 2;
+					view = viewn;
+					Vector3 looktarget = position + 100000 * view;
+					iTween.MoveTo (gameObject, iTween.Hash ("position", position, "time", 1.5f, "orienttopath", false, "lookahead", 0f, /*"looktarget", looktarget,*/"easetype", "easeInOutQuad"));
+					iTween.LookTo (gameObject, iTween.Hash ("looktarget", looktarget, "time", 1.5f));
+				} else if (turnRight) {
+					nextMove = nextMove + 1.7f;
+					Vector3 viewn = gameObject.transform.right.normalized;
+					position = position + (view + viewn) / 2;
+					view = viewn;
+					Vector3 looktarget = position + 100000 * view;
+					iTween.MoveTo (gameObject, iTween.Hash ("position", position, "time", 1.5f, "orienttopath", false, "lookahead", 0f, /*"looktarget", looktarget,*/"easetype", "easeInOutQuad"));
+					iTween.LookTo (gameObject, iTween.Hash ("looktarget", looktarget, "time", 1.5f));
+				} else if (checkField) {
+					nextMove = nextMove + 0.3f;
+					Vector3 field = position + view / 2;
+					int x = Mathf.FloorToInt (field.x);
+					int z = Mathf.FloorToInt (field.z);
+					grid.deleteLidAtPosition (x, z);
+					int state = grid.getGameGridAtPosition (x, z);
+					if (state == 9) {
+						print ("BOOOM!!!");
+						print ("You lost!");
+					}
+					if (grid.isFinished ()) {
+						print ("You won!");
+					}
+				} else if (flagField) {
+					nextMove = nextMove + 0.3f;
+					Vector3 field = position + view / 2;
+					int x = Mathf.FloorToInt (field.x);
+					int z = Mathf.FloorToInt (field.z);
+					bool flagable = grid.hasLidAtPosition (x, z);
+					if(flagable){
+						GameObject flagObject = Instantiate (flagPrefab, new Vector3 (field.x, 0.1f, field.z), Quaternion.identity) as GameObject;
+						flagObject.transform.parent = GameObject.FindWithTag ("GameArea").GetComponent<Transform> ();
+
+						GameObject minimapFlag = Instantiate (flagSprite, new Vector3 (field.x, 0.2f, field.z), flagSprite.transform.rotation) as GameObject;
+						minimapFlag.transform.parent = GameObject.FindWithTag ("GameArea").GetComponent<Transform> ();
+					}
+					//flagObject.animation.Play();
+>>>>>>> created and added FlagSprite for Minimap
 				}
 
 		}
