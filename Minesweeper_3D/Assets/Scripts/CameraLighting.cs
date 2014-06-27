f7abe58e8b6e629a041b4cb7c9f411ab131b36ca
Maximilian Zeigert <madalga07@hotmail.de>
2014-06-27 07:40:16 +0200

    Added Flashlight for Character, directional light is now only for minimap, sripts to handle visibility of light, StandardAssets Light Cookies for Flashlight-Optic

---
+++ Minesweeper_3D/Assets/Scripts/CameraLighting.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLighting : MonoBehaviour {
+
+	public Light minimapLighting;
+	public Light flashlight;
+
+	void OnPreRender () {
+		flashlight.enabled = false;
+	}
+
+	void OnPostRender () {
+		minimapLighting.enabled = true;
+	}
+}
