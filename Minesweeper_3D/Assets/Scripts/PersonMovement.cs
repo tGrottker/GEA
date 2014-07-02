274ade1dd418e78f315308749046561b241b5054
Maximilian Zeigert <madalga07@hotmail.de>
2014-07-02 20:24:25 +0200

    - added trees - added additional Scene - WoodAmbient

--- Minesweeper_3D/Assets/Scripts/PersonMovement.cs
+++ Minesweeper_3D/Assets/Scripts/PersonMovement.cs
@@ -21,7 +21,7 @@ public class PersonMovement : MonoBehaviour
 		view = gameObject.transform.forward.normalized;
 		grid = plane.GetComponent <GameGrid> ();
 		nextMove = 0;
-		pauseMenu = GameObject.Find ("Main Camera").GetComponent<PauseMenu> ();
+		pauseMenu = GameObject.FindWithTag("MainCamera").GetComponent<PauseMenu> ();
 	}
 
 	// Update is called once per frame
@@ -33,9 +33,9 @@ public class PersonMovement : MonoBehaviour
 		bool checkField = Input.GetMouseButtonDown (0);
 		bool flagField = Input.GetMouseButtonDown (1);
 	
-		if (pauseMenu.paused) {
+		/*if (pauseMenu.paused) {
 			return;
-		}else{
+		}else{*/
 			if (nextMove < Time.time) {
 				if (stepForward) {
 					nextMove = nextMove + 1.2f;
@@ -91,7 +91,7 @@ public class PersonMovement : MonoBehaviour
 					}
 				}
 			}
-		}
+		//}
 	}
 
 	public Vector3 getCurrentField () {
