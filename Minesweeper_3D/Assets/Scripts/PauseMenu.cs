1b62e0e8cc222a12370ffea42804ac30b3eaf4c8
Thomas Grottker <s52388@beuth-hochschule.de>
2014-07-02 15:00:19 +0200

    added pause menu with ability to exit the game

---
+++ Minesweeper_3D/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+	private int groupWidth = 200;
+	private int groupHeight = 170;
+	public bool paused = false;
+	
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;
+		Time.timeScale = 1;
+	}
+	
+	void OnGUI() {
+		if (paused) {
+			GUI.BeginGroup(new Rect(((Screen.width/2) - (groupWidth/2)), ((Screen.height/2) - (groupHeight/2)), groupWidth, groupHeight));
+			if (GUI.Button(new Rect(0, 0, buttonWidth, buttonHeight), "Main Menu")) {
+				print("Exiting game and going to main menu");
+				// TODO Application.LoadLevel(0);
+			}
+			if (GUI.Button(new Rect(0, 60, buttonWidth, buttonHeight), "Restart Game")) {
+				print("Restarting game");
+				// TODO Application.LoadLevel(1);
+			}
+			if (GUI.Button(new Rect(0, 120, buttonWidth, buttonHeight), "Exit Game")) {
+				Application.Quit();
+			}
+			GUI.EndGroup();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			paused = togglePause();
+		}
+	}
+	
+	private bool togglePause() {
+		if (Time.timeScale == 0) {
+			// unpause
+			Screen.lockCursor = true;
+			Time.timeScale = 1;
+			return false;
+		} else {
+			// pause
+			Screen.lockCursor = false;
+			Time.timeScale = 0;
+			return true;
+		}
+	}
+	
+}
