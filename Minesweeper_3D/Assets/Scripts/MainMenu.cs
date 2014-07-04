3181f60756975e5ecb5dec54ea3236b31c5a2be4
Thomas Grottker <s52388@beuth-hochschule.de>
2014-07-04 21:28:46 +0200

    added working main menu and empty credits page

---
+++ Minesweeper_3D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+	private int groupWidth = 200;
+	private int groupHeight = 170;
+
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = false;
+	}
+	
+	void OnGUI () {
+		GUI.BeginGroup (new Rect (((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeight / 2)), groupWidth, groupHeight));
+		if (GUI.Button (new Rect (0, 0, buttonWidth, buttonHeight), "Start Game")) {
+			Application.LoadLevel ("Main_Scene");
+		}
+		if (GUI.Button (new Rect (0, 60, buttonWidth, buttonHeight), "Credits")) {
+			Application.LoadLevel ("Credits");
+		}
+		if (GUI.Button (new Rect (0, 120, buttonWidth, buttonHeight), "Exit Game")) {
+			Application.Quit ();
+		}
+		GUI.EndGroup ();
+	}
+}
