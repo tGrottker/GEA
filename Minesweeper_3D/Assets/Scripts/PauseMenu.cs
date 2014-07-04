7b23573d0718c918d4dde6456b5be8aa8a5f4938
Thomas Grottker <s52388@beuth-hochschule.de>
2014-07-04 20:50:57 +0200

    added restart feature

--- Minesweeper_3D/Assets/Scripts/PauseMenu.cs
+++ Minesweeper_3D/Assets/Scripts/PauseMenu.cs
@@ -20,7 +20,7 @@ public class PauseMenu : MonoBehaviour {
 			GUI.BeginGroup(new Rect(((Screen.width/2) - (groupWidth/2)), ((Screen.height/2) - (groupHeight/2)), groupWidth, groupHeight));
 			if (GUI.Button(new Rect(0, 0, buttonWidth, buttonHeight), "Restart Game")) {
 				print("Restarting game");
-				// TODO Application.LoadLevel(1);
+				Application.LoadLevel("Main_Scene");
 			}
 			if (GUI.Button(new Rect(0, 60, buttonWidth, buttonHeight), "Back to Mainmenu")) {
 				print("Exiting game and going to main menu");
