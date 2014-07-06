cdf85ecd179a143deee6daccd63e878653a1f80c
Thomas Grottker <s52388@beuth-hochschule.de>
2014-07-06 15:34:00 +0200

    added credits

---
+++ Minesweeper_3D/Assets/Scripts/Credits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnGUI() {
+		if (GUI.Button (new Rect (280, 400, buttonWidth, buttonHeight), "Back to Mainmenu")) {
+			Application.LoadLevel("Main_Menu");
+		}
+	}
+}
